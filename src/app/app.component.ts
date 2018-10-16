3e2c48ba0f2981682d288d54d3838a9681451ae6
Newton Kiragu <karanunewton4@gmail.com>
2018-10-16 04:41:47 +0300

    created a goal class and displayed all goals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Goal} from './goal';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'goals';
+  title = 'Goals';
+  goal = 'Watch finding Nemo';
+  goals = [
+    new Goal(1, 'I am a funky monkey'),
+    new Goal(2, 'do some stuff here and there'),
+    new Goal(3, 'read eleven minutes'),
+    new Goal(4, 'read twelve minutes'),
+    new Goal(5, 'read thirteen minutes'),
+    new Goal(6, 'read fourteen minutes')
+  ]
 }
---
+++ src/app/goal.ts
@@ -0,0 +1,6 @@
+export class Goal {
+    constructor(
+        public id:number,
+        public name:string
+    ){}
+}
