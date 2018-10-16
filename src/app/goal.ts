a14fa0301ab712fcaeca2adceb33189b37641bfc
Newton Kiragu <karanunewton4@gmail.com>
2018-10-16 04:56:27 +0300

    added descriptions to goals and created goal-child component to display the descriptions

--- src/app/goal.ts
+++ src/app/goal.ts
@@ -1,6 +1,7 @@
 export class Goal {
     constructor(
         public id:number,
-        public name:string
+        public name:string,
+        public description:string
     ){}
 }
--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -8,12 +8,12 @@ import { Goal } from '../goal';
 })
 export class GoalComponent implements OnInit {
   goals = [
-    new Goal(1, 'I am a funky monkey'),
-    new Goal(2, 'do some stuff here and there'),
-    new Goal(3, 'read eleven minutes'),
-    new Goal(4, 'read twelve minutes'),
-    new Goal(5, 'read thirteen minutes'),
-    new Goal(6, 'read fourteen minutes')
+    new Goal(1, 'I am a funky monkey', 'this is not really a goal, just a comment'),
+    new Goal(2, 'do some stuff here and there', 'there is a lotta stuff to be done'),
+    new Goal(3, 'read eleven minutes', 'cause i am still at page 43 and i have two novels to read'),
+    new Goal(4, 'read twelve minutes', 'this is a non existent book'),
+    new Goal(5, 'read thirteen minutes', 'this goal might never be attained'),
+    new Goal(6, 'read fourteen minutes', 'this sounds like a grammatical error')
   ]
   constructor() { }
 
